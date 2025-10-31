4647291f461ac532b5dd819785b9fae86db331a4
VinayNCLK <vinayn348@gmail.com>
2025-10-31 08:32:26 +0530

    31/10

---
+++ src/test/java/com/AutomationFrameowork/listeners/CustomListener.java
@@ -0,0 +1,60 @@
+package com.AutomationFrameowork.listeners;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.testng.Reporter;
+
+public class CustomListener implements ITestListener{
+
+	  public void onTestStart(ITestResult result) {
+		    // not implemented
+		  }
+
+		  public void onTestSuccess(ITestResult result) {
+		    // not implemented
+			  //Reporter.log("<a href='file:///C:/Users/Admin/Desktop/Slenium/fb-inputbox.png'>"
+				//  		+ "<img src='file:///C:/Users/Admin/Desktop/Slenium/fb-inputbox.png' height=100 width=100> </a>", true);
+			  System.setProperty("org.uncommons.reportng.escape-output","false");
+			 Reporter.log("<a href=\"file:///C:/Users/Admin/Desktop/Slenium/fb-inputbox.png\" target=\"_blank\">click here to see the sc</a>");
+			  Reporter.log("Test case passed", true);	
+		  }
+
+
+		  public void onTestFailure(ITestResult result) {
+		    // not implemented
+			  System.setProperty("org.uncommons.reportng.escape-output","false");
+			 // Reporter.log("<a href='file:///C:/Users/Admin/Desktop/Slenium/fb-inputbox.png'>"
+			 //		+ "<img src='file:///C:/Users/Admin/Desktop/Slenium/fb-inputbox.png' height=100 width=100> </a>", true);
+			  
+			  
+			  Reporter.log("<a href=\"file:///C:/Users/Admin/Desktop/Slenium/fb-inputbox.png\" target=\"_blank\"> click here </a>");
+			  Reporter.log("Test case failed", true);	
+		  }
+
+
+		  public void onTestSkipped(ITestResult result) {
+		    // not implemented
+			  Reporter.log("Test case skipped", true);	
+		  }
+
+
+		  public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		    // not implemented
+		  }
+
+
+		  public void onTestFailedWithTimeout(ITestResult result) {
+		    onTestFailure(result);
+		  }
+
+
+		  public void onStart(ITestContext context) {
+		    // not implemented
+		  }
+
+	
+		  public void onFinish(ITestContext context) {
+		    // not implemented
+		  }
+}
